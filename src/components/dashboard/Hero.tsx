e03792c906e36e59cb9bd98dd1a2340f286548e1
telecasteren <127958662+telecasteren@users.noreply.github.com>
2026-09-15 19:29:44 +0200

    style/fix: remove ugly carousel shadow

--- src/components/dashboard/Hero.tsx
+++ src/components/dashboard/Hero.tsx
@@ -9,20 +9,10 @@ const StyledBox = styled("div")(({ theme }) => ({
   height: 400,
   marginTop: theme.spacing(6),
   borderRadius: (theme.vars || theme).shape.borderRadius,
-  outline: "6px solid",
-  outlineColor: "hsla(220, 25%, 80%, 0.2)",
-  border: "1px solid",
-  borderColor: (theme.vars || theme).palette.grey[200],
-  boxShadow: "0 0 12px 8px hsla(220, 25%, 80%, 0.2)",
   [theme.breakpoints.up("sm")]: {
     marginTop: theme.spacing(6),
     height: 600,
   },
-  ...theme.applyStyles("dark", {
-    boxShadow: "0 0 24px 12px hsla(210, 100%, 25%, 0.2)",
-    outlineColor: "hsla(220, 20%, 42%, 0.1)",
-    borderColor: (theme.vars || theme).palette.grey[700],
-  }),
 }));
 
 export function Hero() {
